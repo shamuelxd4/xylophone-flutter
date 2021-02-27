38635fc4e401b612e42fe4fafa36bde28675c0e7
Samuel Quenta Carvajal <50058913+shamuelxd4@users.noreply.github.com>
2021-02-27 14:51:31 -0400

    migrate to flutter2 and update to flie build.gradle the vesion of the android how support

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -6,9 +8,18 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                AudioCache player = AudioCache();
+                player.play('note2.wav');
+              },
+              child: Text('press go..!'),
+            ),
+          ),
         ),
       ),
     );
