d1a1d401a09b8bd043d0eeaef86c3cc5ed20bebb
Samuel Quenta Carvajal <50058913+shamuelxd4@users.noreply.github.com>
2021-02-28 23:06:31 -0400

    desarrollo finalizado con manejo de dart package y mejorando el codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
@@ -10,18 +9,39 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                AudioCache player = AudioCache();
-                player.play('note2.wav');
-              },
-              child: Text('press go..!'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildFlatButton(numero: 1, color: Colors.red),
+              buildFlatButton(numero: 2, color: Colors.orange),
+              buildFlatButton(numero: 3, color: Colors.yellow),
+              buildFlatButton(numero: 4, color: Colors.green),
+              buildFlatButton(numero: 5, color: Colors.blue),
+              buildFlatButton(numero: 6, color: Colors.indigo),
+              buildFlatButton(numero: 7, color: Colors.purple),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Widget buildFlatButton({int numero, Color color}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(numero);
+        },
+        child: null,
+      ),
+    );
+  }
+
+  void playSound(int numero) {
+    AudioCache player = AudioCache();
+    player.play('note$numero.wav');
   }
 }
